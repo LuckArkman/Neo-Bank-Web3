eb2bbd3d6571143698494d06047741723f00389a
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-12-30 13:16:10 -0300

    main

---
+++ Bank.MobileUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Bank.MobileUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Bank.MobileUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Bank.MobileUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Bank.MobileUI.Pages")]

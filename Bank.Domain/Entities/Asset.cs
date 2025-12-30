098a79ea958c8041629aac9e95c79a1f1fb95c31
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-12-30 14:28:58 -0300

    up

---
+++ Bank.Domain/Entities/Asset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Domain.Entities
+{
+    public class Asset
+    {
+        public string Symbol { get; set; } // EX: ETH, MATIC
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public decimal UsdValue { get; set; }
+        public string LogoUrl { get; set; }
+    }
+    
+}
---
+++ Bank.Domain/Interfaces/IWalletService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Domain.Interfaces
+{
+    public interface IWalletService
+    {
+        Task<string> GetOrCreateWalletAsync(); // Retorna o endereço 0x...
+        Task<decimal> GetNativeBalanceAsync(string address);
+        Task<string> SendTransactionAsync(string toAddress, decimal amount);
+        Task<bool> StakeAssetAsync(decimal amount); // Função de Staking
+    }
+}
